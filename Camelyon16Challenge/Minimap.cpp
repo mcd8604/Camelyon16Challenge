8a5f45ec2c781f45f0f5103d97eac64cddfda738
Mike DeMauro <mcd8604@gmail.com>
2015-12-31 11:37:45 -0500

    Minimap test - Fixed wait and window

--- Camelyon16Challenge/Minimap.cpp
+++ Camelyon16Challenge/Minimap.cpp
@@ -17,9 +17,11 @@ Minimap::~Minimap()
 
 int main(int argc, char **argv) {
 
+	namedWindow("Minimap", WINDOW_KEEPRATIO);
+
 	TepisClient tepisClient("https://tepis.ctmm-trait.nl/");
 	// TODO externalize credentials
-	tepisClient.authenticate("username", "password");
+	tepisClient.authenticate("demaurm", "kJJC72JF2!jf@");
 	string imageID = "1.3.46.670589.45.1.1.118986034794496.1.4884.1448292561733.2";
 
 	// Get Metadata
@@ -29,11 +31,15 @@ int main(int argc, char **argv) {
 
 	// Get ImagePixelData
 	vector<unsigned char> imagePixelData = tepisClient.getImagePixelData(imageID, 0, 0, 512, 1024, 8, Unit::PIXEL);
-	Mat image = imdecode(imagePixelData, CV_LOAD_IMAGE_ANYDEPTH);
-	imshow("Minimap", image);
+	Mat image = imdecode(imagePixelData, CV_LOAD_IMAGE_ANYCOLOR);
+	while (waitKey(0) != 27) //esc
+	{
+		imshow("Minimap", image);
+	}
+
 
 	// Wait indefinitely
-	pplx::wait(concurrency::event::timeout_infinite);
+	//pplx::wait(concurrency::event::timeout_infinite);
 
 	// Cleanup all windows
 	destroyAllWindows();
