a6d1a158713dee4f330a8ceb37813cd650dbba8d
Mike DeMauro <mcd8604@gmail.com>
2016-01-14 20:51:30 -0500

    Added binary threshold.

--- Camelyon16Challenge/Minimap.cpp
+++ Camelyon16Challenge/Minimap.cpp
@@ -1,6 +1,7 @@
 #include "Minimap.h"
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 using namespace tepis;
 using namespace std;
@@ -15,8 +16,8 @@ Minimap::~Minimap()
 {
 }
 
-int main(int argc, char **argv) {
 
+int main(int argc, char **argv) {
 	namedWindow("Minimap", WINDOW_KEEPRATIO);
 
 	TepisClient tepisClient("https://tepis.ctmm-trait.nl/");
@@ -25,16 +26,28 @@ int main(int argc, char **argv) {
 	string imageID = "1.3.46.670589.45.1.1.118986034794496.1.4884.1448292561733.2";
 
 	// Get Metadata
-	ImageMetadata imageMetadata = tepisClient.getImageMetadata(imageID);
+	ImageMetadata imageMetadata;
+	imageMetadata = tepisClient.getImageMetadata(imageID);
 	PixelMetadata pixelMetadata = imageMetadata.getPixelMetadata();
 	vector<PixelLevelMetadata> &levels = pixelMetadata.getLevels();
 
 	// Get ImagePixelData
 	vector<unsigned char> imagePixelData = tepisClient.getImagePixelData(imageID, 0, 0, 512, 1024, 8, Unit::PIXEL);
-	Mat image = imdecode(imagePixelData, CV_LOAD_IMAGE_ANYCOLOR);
-	while (waitKey(0) != 27) //esc
+	Mat image = imdecode(imagePixelData, CV_LOAD_IMAGE_COLOR);
+
+	// PreProcess Image
+	Mat grayImage;
+	grayImage.create(image.size(), image.type());
+	cvtColor(image, grayImage, COLOR_BGR2GRAY);
+	//cvtColor(imagePixelData, grayImage, CV_RGB2GRAY);
+	int thresholdFeature = 160;
+	threshold(grayImage, grayImage, thresholdFeature, 255, 0);
+
+	bool s = true;
+	while (s || waitKey(0) != 27) //esc
 	{
-		imshow("Minimap", image);
+		s = false;
+		imshow("Minimap", grayImage);
 	}
 
 
